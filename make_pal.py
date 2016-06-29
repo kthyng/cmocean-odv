a4a8560f99702addda797c69100f32973dab3aa2
Kristen M. Thyng <kthyng@gmail.com>
2016-06-29 10:04:33 -0500

    script to make .pal files and the files themselves

---
+++ make_pal.py
@@ -0,0 +1,54 @@
+'''
+Make ODV .pal colormap files from cmocean rgb files.
+This reads in the colormap rgb from github directly, and it is stored
+locally by np.genfromtxt.
+'''
+
+import numpy as np
+import os
+import cmocean
+
+
+# number of levels for colormap, set by ODV
+N = 113
+
+# location of local rgb files
+# Update to matplotlib from kthyng once matplotlib is updated.
+loc = 'https://raw.githubusercontent.com/kthyng/cmocean/master/cmocean/rgb/'
+
+# file list
+Files = [loc + name + '-rgb.txt' for name in cmocean.cm.cmapnames]
+
+if not os.path.exists:
+    os.makedirs('pal')
+
+# Loop through rgb files and make pal file
+for File in Files:
+
+    # read in rgb values
+    rgb = np.genfromtxt(File)
+
+    # convert to colormap
+    cmap = cmocean.tools.cmap(rgb, N=N)
+
+    # back to rgb, now correct number of levels
+    rgb = cmocean.tools.print_colormaps([cmap], N=N)[0]
+
+    # file name
+    fname = File.split('/')[-1].split('-')[0]
+    f = open('pal/' + fname + '.pal', 'w')
+
+    # fill in lines 0-31 from ODV default palette
+    odv = np.loadtxt('pal/Odv.pal')
+    for j in range(32):
+        f.write('%3i  %.3f  %.3f  %.3f\n' % (odv[j, 0], odv[j, 1], odv[j, 2], odv[j, 3]))
+
+    # loop through rgb to write to file
+    for j in range(rgb.shape[0]):
+        f.write('%3i  %.3f  %.3f  %.3f\n' % (j+32, rgb[j, 0], rgb[j, 1], rgb[j, 2]))
+
+    # fill in lines 145-176
+    for j in range(145, 177):
+        f.write('%3i  %.3f  %.3f  %.3f\n' % (odv[j, 0], odv[j, 1], odv[j, 2], odv[j, 3]))
+
+    f.close()
